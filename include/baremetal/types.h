3d538b73d85b5dc84f88299aa594c49422db8577
Taylor Holberton <taylorcholberton@gmail.com>
2017-10-11 19:24:34 -0400

    added types file

---
+++ include/baremetal/types.h
@@ -0,0 +1,25 @@
+// =======================================================================
+// BareMetal Standard C Library -- The standard C library for BareMetal OS
+// Copyright (C) 2008-2017 Return Infinity -- see LICENSE
+//
+// Version 1.0
+// =======================================================================
+
+/// @file
+
+#ifndef BAREMETAL_LIBC_TYPES_H
+#define BAREMETAL_LIBC_TYPES_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/// @brief An unsigned integer that
+/// can represent the size of any object.
+typedef unsigned long long int size_t;
+
+#ifdef __cplusplus
+} // extern "C" {
+#endif
+
+#endif // BAREMETAL_LIBC_TYPES_H
