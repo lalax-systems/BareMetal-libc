0a29b247d956f62b78345207f9e5bcc232545f62
Taylor Holberton <taylorcholberton@gmail.com>
2017-10-11 20:50:00 -0400

    added stdlib.h

---
+++ include/stdlib.h
@@ -0,0 +1,31 @@
+// =======================================================================
+// BareMetal Standard C Library -- The standard C library for BareMetal OS
+// Copyright (C) 2008-2017 Return Infinity -- see LICENSE
+//
+// Version 1.0
+// =======================================================================
+
+#ifndef BAREMETAL_LIBC_STDLIB_H
+#define BAREMETAL_LIBC_STDLIB_H
+
+#include <baremetal/types.h>
+
+/** @brief Returned by main if an error
+ * occured within the program.
+ * */
+#define EXIT_FAILURE 1
+
+/** @brief Returned by main if a program
+ * exited without any errors.
+ * */
+#define EXIT_SUCCESS 0
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+} // extern "C"
+#endif
+
+#endif // BAREMETAL_LIBC_STDLIB_H
